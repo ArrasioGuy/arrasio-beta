b0abee75c3d2f6d9074eb20adb288920feadf834
ContentArras <151041462+ArrasioGuy@users.noreply.github.com>
2025-01-31 13:20:17 +0100

    Prob not fixed server -> Unknown Server

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -490,7 +490,7 @@ global.time = 0;
 
 // Window setup <3
 global.mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
-var serverName = 'Prob Not Fixed Server';
+var serverName = 'Unknown Server';
 window.onload = () => {
     // Server name stuff
     switch (window.location.hostname) {
